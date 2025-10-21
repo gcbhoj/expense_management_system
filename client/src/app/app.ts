e78e24be56075136be74eccbd7a3bd89a6ba2db2
BhojGC <gcbhoj@gmail.com>
2025-10-21 19:25:54 -0400

    navbar design complete needs to complete routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { ExpenseManager } from './components/expense-manager/expense-manager';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('client');
