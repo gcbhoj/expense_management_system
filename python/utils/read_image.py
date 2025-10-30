5b7b2462039de5f85ba8f44b9a0e9961430e5845
BhojGC <gcbhoj@gmail.com>
2025-10-29 23:38:29 -0400

    Scessufly read total from the receipt image image not processed yet using the test.py in utils to test retreiveing complete receipt text

--- python/utils/read_image.py
+++ python/utils/read_image.py
@@ -1,14 +1,21 @@
 import pytesseract
-from PIL import Image, ImageFilter, ImageEnhance
-
+from PIL import Image, ImageFilter, ImageEnhance,ImageOps
+import re
+import os 
+# //processsing the image
 def preprocess_image(image_path):
     # Convert to grayscale
     image = Image.open(image_path).convert("L")
-    # Sharpen the image
-    image = image.filter(ImageFilter.SHARPEN)
+    # # Sharpen the image
+    image = image.filter(ImageFilter.SMOOTH)
     # Increase contrast
     enhancer = ImageEnhance.Contrast(image)
-    image = enhancer.enhance(2.0)
+    image = enhancer.enhance(2)
+        # Optional: Invert colors if background is dark
+    image = ImageOps.invert(image) if image.getextrema()[0] < 128 else image
+    
+    # Denoise slightly instead of oversharpening
+    image = image.filter(ImageFilter.MedianFilter(size=3))
     return image
 
 def ocr_image(image_path, tesseract_cmd=None):
@@ -19,10 +26,22 @@ def ocr_image(image_path, tesseract_cmd=None):
     text = pytesseract.image_to_string(image, lang='eng')
     return text
 
-# === Main Program ===
-image_path = "receipt.jpg"
-tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# the following function extracts the total amount from the receipt and sents it to the main java controller
+def extract_total_expense():
 
-extracted_text = ocr_image(image_path, tesseract_cmd=tesseract_cmd)
-print("Extracted Text:\n")
-print(extracted_text)
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(BASE_DIR, "receipt.jpg")
+
+    tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+    extracted_text = ocr_image(image_path, tesseract_cmd=tesseract_cmd)
+    extracted_text = extracted_text.lower()  # Convert to lowercase
+    # # extracted_text = re.sub(r'http\S+', '', extracted_text)  # Remove URLs
+    # extracted_text = re.sub(r'[^\w\s]', '', extracted_text)  # Remove punctuation
+    extracted_text = re.sub(r'\s+', ' ', extracted_text).strip()  
+
+    total_expense_match = re.search(r'total[:\s]*\$?(\d+\.\d{2})', extracted_text)
+    # print("Total Expense Found:", total_expense_match.group(1) if total_expense_match else "Not Found")
+    # print("Extracted Text:\n")
+    # print(extracted_text)
+    return total_expense_match
